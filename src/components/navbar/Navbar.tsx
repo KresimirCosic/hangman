d7b7a2ddfeea738e3d4dd7d36515ba30631fde47
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2022-02-21 16:23:59 +0100

    navbar basic styles just some scss rules to see if all's working

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,25 @@
 import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './Navbar.scss';
+
 const Navbar: FC = () => {
   return (
     <div id='Navbar' className='Navbar'>
-      <ul>
-        <li>
-          <NavLink to={'/login'}>Login</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/'}>Home</NavLink>
-        </li>
-      </ul>
+      <nav>
+        <ul className='Navbar-list'>
+          <li className='Navbar-list-item'>
+            <NavLink className='Navbar-list-item-link' to={'/login'}>
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className='Navbar-list-item-link' to={'/'}>
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
